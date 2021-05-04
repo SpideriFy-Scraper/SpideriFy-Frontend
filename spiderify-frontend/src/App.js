9a3394bdac081e7ff8d4fda1ec23c9475943e1ac
ehsan <thehead4ever@gmail.com>
2021-05-04 12:16:22 +0300

    configured the basic project , added ESlint and bootstrap

--- spiderify-frontend/src/App.js
+++ spiderify-frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React  from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Button , Badge } from 'reactstrap'
+
+
 
 function App() {
+
+function buttonClicked (){
+  console.log("button clicked");
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {{backgroundImage : `url('spiderify_sans_search.png')` , backgroundRepeat : 'no-repeat'   }}  className="App-logo">
+
+      <div style = {{float : 'left' , padding : '10px 0px 0px 10px'}}>
+        <Button color = "danger" style ={{align : "left"}} onClick = {buttonClicked}> Contact us </Button>{' '}
+        <Button color = "danger" > FAQ </Button>{' '}
+        <Button color = "danger" > about </Button>
+      </div>
+
+      <div style ={{clear : 'both'}}>
+        <h1> <Badge color = "secondary"> Comments in your hands </Badge> </h1>
+
+        <div style = {{margin : '27% 30% 0% 30%'}}>
+        <Button outline block color = "secondary" style={{color :'silver'}} onClick = {buttonClicked}><h2 style = {{padding : '1%'}}> Getting started </h2></Button>{' '}
+        </div>
+      </div>
     </div>
   );
 }
