3ace815da91e04cda73640277273a990db2e9483
ehsan <thehead4ever@gmail.com>
2021-05-05 08:26:31 +0300

    added the gradient div style to the comment component

--- spiderify-frontend/src/App.js
+++ spiderify-frontend/src/App.js
@@ -28,7 +28,16 @@ const testCommentObject1 = {
   "rating": 4.0,
   "sentiment": "0.9993892908096313"
 }
-
+const testCommentObject2 = {
+  "title": "The computer tutorials along with \"Cortana\" open doors some of us never imagined...",
+  "content": "This is my first computer. I wasn't sure what to expect but I do know that I was not prepared for what was in that carefully wrapped package. I chose a business computer because of the reviews, herein, which spoke to how you get so much more at a better price and with less fluff...what an understatement, that was! Between the excellent tutorials, \"Cortana,\" various upgrades, and a machine that seems almost intuitive when it comes to offering just what you need-for free-right when you needed it, I was almost overwhelmed with the creative possibilities so much so that I still can't get this grin off of my face! ",
+  "date": "Reviewed in the United States on September 25, 2020",
+  "variant": null,
+  "verified": true,
+  "author": "Joseph R.",
+  "rating": 5.0,
+  "sentiment": "0.9989765286445618"
+}
 function buttonClicked (){
   console.log("button clicked");
 }
@@ -61,7 +70,7 @@ function buttonClicked (){
         <Route path = '/Comment-test'>
       <Comment commentSpec = {testCommentObject} ></Comment>
       <Comment commentSpec = {testCommentObject1}></Comment>
-
+      <Comment commentSpec = {testCommentObject2}></Comment>
         </Route>
       </Switch>
         </div>
--- spiderify-frontend/src/components/Comment.jsx
+++ spiderify-frontend/src/components/Comment.jsx
@@ -39,7 +39,7 @@ class Comment extends Component {
 function CommentFunctional(props)
 {
     return (
-        <div style = {{backgroundColor : 'rgb(4, 158, 209)' , marginBottom : '8px' , marginLeft : '14%' , marginRight : '14%' , borderRadius : '25px'  }}>
+        <div style = {{backgroundImage : 'linear-gradient(to bottom , rgb(4 , 98 , 209) , rgb(0, 189, 255)' , marginBottom : '8px' , marginLeft : '14%' , marginRight : '14%' , borderRadius : '25px'  }}>
             <div >
                 <h4 style = {{float : 'left' , marginTop : '1%' , marginLeft : '1%'}}>
                     {props.commentSpec.author} : {props.commentSpec.title}
@@ -47,7 +47,7 @@ function CommentFunctional(props)
                 <p style = {{float : "right" , marginTop : '1%' , marginRight : '1%' }}>
                     {props.commentSpec.date}
                 </p>
-                <div style = {{clear : 'both' , backgroundColor : 'silver' , margin : '5px' , borderRadius : '5px' , color:'black'}}>
+                <div style = {{clear : 'both' , backgroundImage : 'linear-gradient(to bottom right , white , rgb(153, 153, 153)'  , margin : '5px' , borderRadius : '5px' , color:'black'}}>
                         <p >
                             {props.commentSpec.content}
                         </p>
